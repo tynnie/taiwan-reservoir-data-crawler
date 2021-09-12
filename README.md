c5ac8986551e3a6011b3944c8770f83014aea125
Ting Ni Wu <30755520+tynnie@users.noreply.github.com>
2021-09-12 23:10:08 +0800

    add meta data

--- README.md
+++ README.md
@@ -1,15 +1,12 @@
 # 歷年水庫水情資料爬取
 
-2020年台灣梅雨季降雨不豐，加上遭逢56年來罕見夏季無颱風登陸的窘境，各地水庫紛紛見底，旱象至今未見緩解跡象。這場旱災有多嚴重？哪邊的水庫最渴？為了解答這些問題，就必須先拿到各地水庫的水情資料。
-
 ### 資料搜集方式
 用Python的selenium自動化從「[台灣地區主要水庫蓄水量報告表](https://fhy.wra.gov.tw/ReservoirPage_2011/StorageCapacity.aspx)」下載歷年資料。
 
-### 資料限制
-資料中並無水庫的地理位置資料，若要結合地理特徵，需另結合[水庫的地理資料](https://gic.wra.gov.tw/Gis/Gic/DataIndex/Data/Main.aspx)。
-
 ### 參考資料
-- [GitHub]infographicstw/[reservoir-history-crawler專案](https://github.com/infographicstw/reservoir-history-crawler)
 
-- 《天下雜誌》「[即時水情地圖》下雨了，台灣水庫解渴了嗎？｜互動專題｜天下雜誌](https://web.cw.com.tw/drought-2021/index.html)
-」
+- [data][水庫基本資料](https://data.gov.tw/dataset/32726)
+  
+- [GitHub][infographicstw/reservoir-history-crawler](https://github.com/infographicstw/reservoir-history-crawler)
+
+- [GitHub][tasi788/TaiwanReservoir](https://github.com/tasi788/TaiwanReservoir)
--- crawler.py
+++ crawler.py
@@ -1,4 +1,7 @@
+import logging
+import os
 from time import sleep
+from datetime import timezone, datetime, timedelta
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.support.select import Select
@@ -8,57 +11,154 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
 
+# path setting
+dir_path = os.path.dirname(os.path.realpath(__file__))
+directory = ['/output/', '/log/']
+for d in directory:
+    if not os.path.exists(dir_path + d):
+        os.makedirs(dir_path + d)
+
+# webdriver setting
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+
+# datetime setting
+local_tz = timezone(timedelta(hours=+8))
+
+
+def get_time_record():
+    time_record = datetime.now().astimezone(local_tz).strftime('%Y%m%dT%H%M')
+    return time_record
+
+
+def get_yesterday_date():
+    yesterday = datetime.now().astimezone(local_tz) - timedelta(1)
+    date = yesterday.strftime('%Y%m%d')
+    return date
+
+
+def add_logger(timestamp):
+    logging.captureWarnings(True)
+    formatter = logging.Formatter(
+        '[%(asctime)s] [%(levelname)s] [%(message)s]')
+    logging.Formatter.converter = lambda *args: datetime.now(
+        tz=local_tz).timetuple()
+    my_logger = logging.getLogger('my_logger')
+    my_logger.setLevel(logging.INFO)
+
+    # file handler: 設定 logger 儲存的檔案格式
+    filename = '/log/{}.log'.format(timestamp)
+    file_handler = logging.FileHandler(dir_path + filename, 'w', 'utf-8')
+    file_handler.setFormatter(formatter)
+    my_logger.addHandler(file_handler)
+
+    # console handler: 設定 logger 將訊息丟到 console 的格式
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.DEBUG)
+    console_handler.setFormatter(formatter)
+    my_logger.addHandler(console_handler)
+
+    return my_logger
+
+
 def setup_driver(link):
-    driver = webdriver.Firefox(executable_path="the_path_to_your_driver")
-    # driver = webdriver.Chrome(executable_path="the_path_to_your_driver")
+    driver = webdriver.Chrome(executable_path='/Users/tynnie/Documents/chromedriver', chrome_options=chrome_options)
+    # driver = webdriver.Chrome(executable_path="the_path_to_your_driver", chrome_options=chrome_options)
     driver.get(link)
 
     return driver
 
 
+def record_time_parser(record_time):
+    if len(record_time) > 1:
+        parsed = '-'.join([record_time.split('(')[0][:4],
+                           record_time.split('(')[0][4:6],
+                           record_time.split('(')[0][6:]])
+        return parsed
+
+
+def combine_data(data, column):
+    data.columns = column[:-1]
+    df = data.drop(['統計時間', '與昨日水位差(公尺)'], axis=1)
+    df = df.replace(r'\s+|%|％|-+', '', regex=True)
+    df['水情時間'] = (df['水情時間']
+                  .astype(str)
+                  .apply(lambda x: record_time_parser(x)))
+
+    df.columns = ['ReservoirName', 'EffectiveCapacity', 'CatchmentAreaRainfall',
+                  'InflowVolume', 'OutflowTotal', 'RecordTime', 'WaterLevel',
+                  'EffectiveWaterStorageCapacity', 'WaterStorageRate']
+
+    df_meta = pd.read_csv('meta.csv')
+    df_meta = df_meta[['Application', 'Area', 'Location', 'ReservoirName', 'Type', 'Year']]
+
+    df = df.merge(df_meta, on='ReservoirName', how='left')
+    return df
+
+
+def save_data(data, column, date):
+    df_data = combine_data(data, column)
+    file_path = dir_path + '/output/reservoir_{}.csv'.format(date)
+    df_data.to_csv(file_path, index=False)
+
+
 def data_crawler(driver):
-    columns = []
-    data = []
+    date = get_yesterday_date()
+    year = int(date[:4])
+    month = int(date[4:6])
+    day = int(date[6:])
 
-    element = driver.find_element_by_xpath("/html/body/form/div[3]/div[1]/select[1]/option[2]").click()
+    try:
 
-    for y in range(2010, 2022):
+        element = driver.find_element_by_xpath("/html/body/form/div[3]/div[1]/select[1]/option[2]").click()
+        sleep(1)
+
         select = Select(driver.find_element_by_id("ctl00_cphMain_ucDate_cboYear"))
-        select.select_by_value('{}'.format(y))
+        select.select_by_value('{}'.format(year))
+        sleep(1)
 
-        for m in range(1, 13):
-            select = Select(driver.find_element_by_id("ctl00_cphMain_ucDate_cboMonth"))
-            select.select_by_value('{}'.format(m))
+        select = Select(driver.find_element_by_id("ctl00_cphMain_ucDate_cboMonth"))
+        select.select_by_value('{}'.format(month))
+        sleep(1)
 
-            for d in range(1, 32):
-                try:
-                    select = Select(driver.find_element_by_id("ctl00_cphMain_ucDate_cboDay"))
-                    select.select_by_value('{}'.format(d))
-                    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH,
-                                                                                '//*[@id="ctl00_cphMain_btnQuery"]'))).click()
+        select = Select(driver.find_element_by_id("ctl00_cphMain_ucDate_cboDay"))
+        select.select_by_value('{}'.format(day))
+        sleep(1)
 
-                    res = driver.page_source
-                    df = pd.read_html(res)[0]
-                    columns = [i[1] for i in list(df.columns)]
-                    df.columns = columns
-                    data.append(df.iloc[:-1, :-1])
-                    sleep(0.75)
+        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH,
+                                                                    '//*[@id="ctl00_cphMain_btnQuery"]'))).click()
 
-                except StaleElementReferenceException:
-                    pass
+        res = driver.page_source
+        df = pd.read_html(res, flavor='html5lib')[0]
+        columns = [i[1] for i in list(df.columns)]
+        df.columns = columns
+        df = df.iloc[:-1, :-1]
 
-    return data, columns
+        save_data(df, columns, date)
+        driver.quit()
 
+    except StaleElementReferenceException as e:
+        pass
 
-def save_data(d, column):
-    df_data = pd.concat(d)
-    df_data.columns = column[:-1]
-    df_data.to_csv("reservoir_history_data.csv", index=False)
+    except Exception as e:
+        raise e
 
 
 if __name__ == '__main__':
-    url = "https://fhy.wra.gov.tw/ReservoirPage_2011/StorageCapacity.aspx"
-    web_driver = setup_driver(url)
-    all_data = data_crawler(web_driver)
-    save_data(all_data[0], all_data[1])
-    web_driver.quit()
+    timestamp = get_time_record()
+    logger = add_logger(timestamp)
+    logger.info('Start')
+    try:
+        url = 'https://fhy.wra.gov.tw/ReservoirPage_2011/StorageCapacity.aspx'
+        web_driver = setup_driver(url)
+
+        # fetch data
+        data_crawler(web_driver)
+        web_driver.quit()
+
+    except Exception as e:
+        logger.exception("Runtime Error Message:")
+
+    logger.info("Done!")
